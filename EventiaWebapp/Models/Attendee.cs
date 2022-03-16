5fb80d38fccaf74526d28a61bac3395dd9653e45
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-03-16 12:05:46 +0100

    Set up, working tree and just a welcome page.

---
+++ EventiaWebapp/Models/Attendee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventiaWebapp.Models;
+
+public class Attendee
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public virtual ICollection<Event> Events { get; set; }
+}
---
+++ EventiaWebapp/Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventiaWebapp.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public string Address { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public int Spots_Available { get; set; }
+
+        
+        public int OrganizerId { get; set; }
+        public virtual Organizer Organizer { get; set; }
+
+        
+        public virtual ICollection<Attendee> Attendees { get; set; }
+    }
+}
