dc894b25ad839f5489a1602976e9553b09529cda
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-04-04 10:46:04 +0200

    Added Identity

---
+++ EventiaWebapp/Data/EventiaPartTwoDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace EventiaWebapp.Data
+{
+    public class EventiaPartTwoDBContext : IdentityDbContext
+    {
+
+    }
+}
+
+
+
+
---
+++ EventiaWebapp/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EventiaWebapp.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Event> HostedEvents { get; set; }
+        public ICollection<Event> JoinEvents { get; set; }
+    }
+}
